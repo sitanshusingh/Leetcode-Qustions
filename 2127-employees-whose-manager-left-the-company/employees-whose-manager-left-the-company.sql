662b96de926e9ee902a2531982622892ed2c0402
Sitanshu Singh <149856810+sitanshusingh@users.noreply.github.com>
2026-01-17 17:31:45 +0530

    Delete 2127-employees-whose-manager-left-the-company directory

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-SELECT Ep.employee_id
-FROM Employees Ep Left Join Employees Mg ON Ep.manager_id = Mg.Employee_id
-WHERE Ep.SALARY < 30000 AND EP.manager_id IS NOT NULL and Mg.Employee_id is null Order by Ep.employee_id;
